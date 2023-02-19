551a2c43a90bb4a882875ce26b5b3814c5fa016c
Pushkar-Nikumb <109033497+Pushkar-Nikumb@users.noreply.github.com>
2023-02-19 19:34:16 +0530

    Delete factory.js

--- factory.js
+++
@@ -1,9 +0,0 @@
-import web3 from "./web3";
-import campaignFactory from "./build/CampaignFactory.json";
-
-const instance = new web3.eth.Contract(
-  JSON.parse(campaignFactory.interface),
-  "0x732d267EEC9eBa2ddf9442Ba55B86e3018542079"
-);
-
-export default instance;
